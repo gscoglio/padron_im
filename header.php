fc28bb6d1e4b30f9bc29aa3aa1db1498ba57a165
Pablo Scoglio <pabloscoglio@gmail.com>
2012-09-22 15:50:03 -0300

    #3 Altas
    Formulario de Altas avanzado y terminado

--- header.php
+++ header.php
@@ -5,4 +5,7 @@
 <link href="css/myForm.css" rel="stylesheet"/>
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<link type="text/css" href="css/smoothness/jquery-ui-1.8.23.custom.css" rel="Stylesheet" />	
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.8.23.custom.min.js"></script>
 </head>
