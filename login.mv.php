2a7ade69aae675f811c98520f0e721bef81128a7
gscoglio <german.scoglio@gmail.com>
2012-09-24 19:11:05 -0300

    #7 Visualizacion de socios

--- login.mv.php
+++ login.mv.php
@@ -4,28 +4,30 @@
 <body>
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-<legend>Log in</legend>
-<form action="login.php" method="post" class="form-horizontal"><input type="hidden" name="ac" value="log"></input>
-    <div class="control-group">
-        <label class="control-label">Username</label>
-        <div class="controls">
-        <input type="text" id="inputEmail" name="username" placeholder="Username"></input>
+<div class="container">
+    <legend>Log in</legend>
+    <form action="login.php" method="post" class="form-horizontal"><input type="hidden" name="ac" value="log"></input>
+        <div class="control-group">
+            <label class="control-label">Username</label>
+            <div class="controls">
+            <input type="text" id="inputEmail" name="username" placeholder="Username"></input>
+            </div>
         </div>
-    </div>
-    <div class="control-group">
-        <label class="control-label" for="inputPassword">Password</label>
-        <div class="controls">
-        <input type="password" id="inputPassword" name="password" placeholder="Password"></input>
+        <div class="control-group">
+            <label class="control-label" for="inputPassword">Password</label>
+            <div class="controls">
+            <input type="password" id="inputPassword" name="password" placeholder="Password"></input>
+            </div>
         </div>
-    </div>
-    <div class="control-group">
-        <div class="controls">
-        <label class="checkbox">
-        <input type="checkbox"> Remember me</input>
-        </label>
-        <button type="submit" class="btn" value="Login">Sign in</button>
+        <div class="control-group">
+            <div class="controls">
+            <label class="checkbox">
+            <input type="checkbox"> Remember me</input>
+            </label>
+            <button type="submit" class="btn" value="Login">Sign in</button>
+            </div>
         </div>
-    </div>
-</form>
+    </form>
+</div>
 </body>
 </html>    
--- socio.php
+++ socio.php
@@ -21,23 +21,71 @@ $socio = $sociosModel->getSocioById($_GET['id']);
             <div class="page-header">
                 <h1><?= $socio['nombre'] . " " . $socio['apellido']?><small> (<?= $socio['socio_nro'] ?>)</small></h1>
             </div>
-            <h3><small>N&uacute;mero de Socio: </small><?= $socio['socio_nro'] ?></h3>
-            <h3><small>Socio CAI: </small><?= $socio['socio_cai'] ?></h3>
-            <h3><small>Sexo: </small><?= $socio['sexo'] ?></h3>
-            <h3><small>Categor&iacute;a: </small><?= $socio['categoria'] ?></h3>
-            <h3><small>DNI: </small><?= $socio['dni'] ?></h3>
-            <h3><small>Tel&eacute;fono celular: </small><?= $socio['tel_celular'] ?></h3>
-            <h3><small>Tel&eacute;fono particular: </small><?= $socio['tel_particular'] ?></h3>
-            <h3><small>Tel&eacute;fono laboral: </small><?= $socio['tel_laboral'] ?></h3>
-            <h3><small>E-mail: </small><?= $socio['email'] ?></h3>
-            <h3><small>Fecha de afiliacion: </small><?= $socio['fecha_afiliacion'] ?></h3>
-            <h3><small>Presentado por: </small><?= $socio['presentado_por'] ?></h3>
-            <h3><small>Domicilio: </small><?= $socio['domicilio'] ?></h3>
-            <h3><small>Localidad: </small><?= $socio['localidad'] ?></h3>
-            <h3><small>C&oacute;digo postal: </small><?= $socio['codigo_postal'] ?></h3>
-            <h3><small>Barrio / Zona: </small><?= $socio['barrio_zona'] ?></h3>
-            <h3><small>Ocupaci&oacute;n: </small><?= $socio['ocupacion'] ?></h3>
-            <h3><small>Comentarios: </small><?= $socio['extra_comments'] ?></h3>
+            <div class="row" >
+                <div class="span4" >
+                    <h3><small>N&uacute;mero de Socio: </small><?= $socio['socio_nro'] ?></h3>
+                </div>
+                <div class="span4" >
+                    <h3><small>Socio CAI: </small><?= $socio['socio_cai'] ?></h3>
+                </div>
+                <div class="span4" >
+                    <h3><small>Sexo: </small><?= $socio['sexo'] ?></h3>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="span4" >
+                    <h3><small>Categor&iacute;a: </small><?= $socio['categoria'] ?></h3>
+                </div>
+                <div class="span4" >
+                    <h3><small>DNI: </small><?= $socio['dni'] ?></h3>
+                </div>
+                <div class="span4" >
+                    <h3><small>Fecha de afiliacion: </small><?= $socio['fecha_afiliacion'] ?></h3>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="span4" >
+                    <h3><small>Tel&eacute;fono celular: </small><?= $socio['tel_celular'] ?></h3>
+                </div>
+                <div class="span4" >
+                    <h3><small>Tel&eacute;fono particular: </small><?= $socio['tel_particular'] ?></h3>
+                </div>
+                <div class="span4" >
+                    <h3><small>Tel&eacute;fono laboral: </small><?= $socio['tel_laboral'] ?></h3>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="span6" >
+                    <h3><small>E-mail: </small><?= $socio['email'] ?></h3>
+                </div>
+                <div class="span6" >
+                    <h3><small>Presentado por: </small><?= $socio['presentado_por'] ?></h3>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="span6" >
+                    <h3><small>Domicilio: </small><?= $socio['domicilio'] ?></h3>
+                </div>
+                <div class="span6" >
+                    <h3><small>Localidad: </small><?= $socio['localidad'] ?></h3>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="span6" >
+                    <h3><small>C&oacute;digo postal: </small><?= $socio['codigo_postal'] ?></h3>
+                </div>
+                <div class="span6" >
+                    <h3><small>Barrio / Zona: </small><?= $socio['barrio_zona'] ?></h3>
+                </div>
+            </div>
+            <div class="row" >
+                <div class="span6" >
+                    <h3><small>Ocupaci&oacute;n: </small><?= $socio['ocupacion'] ?></h3>
+                </div>
+                <div class="span6" >
+                    <h3><small>Comentarios: </small><?= $socio['extra_comments'] ?></h3>
+                </div>
+            </div>
         </div>
     </body>
 </html>
