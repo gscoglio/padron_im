cd6382e1e20c94e41214c16d4722392d54f67182
gscoglio <german.scoglio@gmail.com>
2012-10-05 02:17:40 -0300

    #17 Elecciones

---
+++ votacion.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Votaci&oacute;n Autoridades Independiente M&iacute;stico</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link type="text/css" href="css/smoothness/jquery-ui-1.8.23.custom.css" rel="Stylesheet" />
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>    	
+    <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.23.custom.min.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="page-header">
+            <h1>Independiente M&iacute;stico <small>Elecci&oacute;n online de autoridades</small></h1>
+        </div>
+<p class="lead">Algunas consideraciones antes de elegir:</p>
+    <dl>
+        <dt>Que se vota?</dt>
+        <dd>Se votan los vocales de la Agrupacion Independiente Mistico para el proximo anio de mandato.</dd>
+        <dt>Se vota presidente y vicepresidente de la agrupacion?</dt>
+        <dd>No. Ya fueron seleccionados. Se votan los vocales que los van a acompaniar.</dd>
+        <dt>Cuantos vocales tengo que votar?</dt>
+        <dd>11. Pero si asi lo desee puede votar a una menor cantidad.</dd>
+        <dt>Cuantas veces puedo votar?</dt>
+        <dd>1 (UNA). Sin excepcion. No importa si solo seleccione a 1 candidato la primera vez. Ese sera su voto.</dd>
+    </dl>
+        <h2>Ahora si, ya puede elegir a sus candidatos:</h2>
+        <p class="lead">Seleccione sus once:</p>
+        
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">VARELA, Jose Luis</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">MARTINEZ, Luis</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">FERNANDEZ, Antonio</input>
+                </label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">GONZALEZ, Marta</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">FILARDI, Omar</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">VILLAR, Guido</input>
+                </label>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">RATTI, Anibal</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">GONZALEZ, Ricardo</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">MOSCUFO, Santiago</input>
+                </label>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">VAZQUEZ, Leandro</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">DEGLI INOCENTI, Marcelo</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">REINGOLD, Sebastian</input>
+                </label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">OLIVE, Hernan</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">SAVINO, Cristian</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">STARVAGGI, Maximo</input>
+                </label>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">STARVAGGI, Hugo</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">CASTAGNOLA, Juan</input>
+                </label>
+            </div>
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">ALONSO, Silvina</input>
+                </label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span4">
+                <label class="checkbox">
+                    <input type="checkbox" value="">SCOGLIO, Pablo</input>
+                </label>
+            </div>
+            <div class="span4">
+                
+            </div>
+            <div class="span4">
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="offset3">
+                <button  class="btn btn-success btn-large" type="button" data-toggle="modal" data-target="#myModal">ELIJO a los candidatos seleccionados</button>
+             
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
