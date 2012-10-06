27895fb6013cd0a19774fb70da23ad7bf1e46fb2
gscoglio <german.scoglio@gmail.com>
2012-10-06 18:11:13 -0300

    #3 ids en los divs

--- navigation_bar.php
+++ navigation_bar.php
@@ -5,8 +5,8 @@
                 <div class="span8">
                     <a class="brand" >Administraci&oacute;n Independiente M&iacute;stico</a>
                     <ul class="nav">
-                    <li class="active"><a href="index.php">Padr&oacute;n</a></li>
-                    <li><a href="altas.php">Altas</a></li>
+                    <li class="active" id="padronNav"><a href="index.php">Padr&oacute;n</a></li>
+                    <li id="altasNav"><a href="altas.php">Altas</a></li>
                     </ul>
                 </div>
                 <div class="offset1 span2">
