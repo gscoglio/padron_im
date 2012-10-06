11cb475e0e16d260294a0abd77910bbca2dd9235
Pablo Scoglio <pabloscoglio@gmail.com>
2012-10-06 15:25:55 -0300

    #3 Altas
    Altas mejoradas y finalizadas

--- navigation_bar.php
+++ navigation_bar.php
@@ -1,9 +1,9 @@
 <div class="navbar">
     <div class="navbar-inner">
-        <a class="brand" >Padr&oacute;n Independiente M&iacute;stico</a>
+        <a class="brand" >Administraci&oacute;n Independiente M&iacute;stico</a>
         <ul class="nav">
-        <li class="active"><a href="index.php">Padr&oacute;n</a></li>
-        <li><a href="altas.php">Altas</a></li>
+        <li class="active" id="padronNav"><a href="index.php">Padr&oacute;n</a></li>
+        <li id="altasNav"><a href="altas.php">Altas</a></li>
         </ul>
     </div>
 </div>
