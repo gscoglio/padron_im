118e9f86daecca01d723f67d129ef553a4a7995d
gscoglio <german.scoglio@gmail.com>
2012-10-08 11:46:55 -0300

    #17 Pantalla de exito

--- end.php
+++ end.php
@@ -1,3 +1,25 @@
-<?php
-
-echo "VOTO REALIZADO";
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Votaci&oacute;n Autoridades Independiente M&iacute;stico</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/> 
+    <script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="hero-unit" style="margin-top: 20px">
+            <div class="row">
+                <div class="span7">
+                    <h1>Gracias por su voto!</h1>
+                    <br/>
+                    <p>El mismo fue registrado exitosamente.</p>
+                    <p>Ahora, a esperar los resultados!</p>
+                </div>
+                <div class="span3">
+                    <img src="img/pulgar_arriba.png" class="img-rounded"></img>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
