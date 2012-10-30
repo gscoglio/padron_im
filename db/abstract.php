dbffd695e11045c4d9cff24e508e19074a686003
gscoglio <german.scoglio@gmail.com>
2012-10-30 20:39:05 -0300

    #17 Salvando error de db caida

--- db/abstract.php
+++ db/abstract.php
@@ -8,7 +8,11 @@ class Padron_Abstract
     
     public function __construct() {
         if(!isset($this->_db)){
-            $this->_db = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+            $this->_db = @mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+            if (!$this->_db) {
+                header("Location: error.php");
+                exit;
+            }
             mysql_select_db(DB_NAME);
         }
     }   
---
+++ error.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Votaci&oacute;n Autoridades Independiente M&iacute;stico</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/> 
+    <script type="text/javascript" src="bootstrap/js/jquery.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="alert alert-error" style="margin-top: 20px">
+        Experimentamos problemas en el servidor. Esto puede deberse a que mucha gente est&aacute; ingresando en este mismo momento. Por favor, intente de nuevo m&aacute;s tarde. Si el problema persiste contacte a los administradores. Gracias.
+        </div>
+    </div>
+</body>
